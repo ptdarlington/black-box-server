2468310f20cef20b954e1106fea51df0b6d24e7b
imears <maxwellmears@gmail.com>
2017-01-29 21:42:04 -0500

    Load images from Imgur

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const app     = express();
 const path    = require('path');
 const port    = process.env.PORT || 3333;
 
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use('/public', express.static(path.join(__dirname, 'public')))
+
+const imageUrls = [
+  'http://i.imgur.com/558qckm.jpg?1',
+  'http://i.imgur.com/8fYJCto.jpg',
+];
 
 app.get('/container/:id', (req, res) => {
-  res.send(`<img src="/static/${[req.params.id]}.png">`);
+  res.send(`<img src="${imageUrls[req.params.id]}" style="width: 100%">`);
 });
 
 app.listen(port, () => {
